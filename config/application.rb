17168fa2e587c941c08331a3e1b8bd419e92eea3
Lucas Zingano <lucasbz@gmail.com>
2021-02-08 18:05:11 -0300

    Fixes non auto-correctable offenses for RuboCop

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module RecipeBrowser
+  # Base application config class
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
